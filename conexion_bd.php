3479bb4e6c19732d548a6aa2f22701998e0e04a5
1222023 <1222023@utp.edu.pe>
2024-02-22 05:49:59 -0500

    first commit

---
+++ conexion_bd.php
@@ -0,0 +1,6 @@
+
+<?php
+    $conexion=new mysqli("localhost","root","","login","8080");
+    $conexion->set_charset("utf-8");
+?>
+   
---
+++ controlador.php
@@ -0,0 +1,14 @@
+<?php 
+
+if (!empty($_POST["btningresar"])) {
+    if (empty($_POST["usuario"]) and empty($_POST["password"])){
+        echo "los campos esta vacio";
+        #code...
+       } else{
+      #code...
+        
+    }
+        
+}
+
+?>
