a18d2ce0465603dcdcf08ba85ae6b54757620e24
1222023 <1222023@utp.edu.pe>
2024-02-25 01:35:30 -0500

    wtopo

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 
 <html>
-
+xxxxxxx
 <head>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,7 +15,7 @@
 </head>
 
 <body>
-   <img class="wave" src="img/wave1.png">
+   <img class="wave" src="img/wave1x.png">
    <div class="container">
       
    <div class="login-content">
